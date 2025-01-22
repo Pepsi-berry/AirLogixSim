e61ea8b39412d1609146b7a747a8e1b28a023121
CyberObservers <13816750292@163.com>
2025-01-22 19:52:37 +0800

    implement get_observations and modify modeling

--- env/MultiAgentEnv.py
+++ env/MultiAgentEnv.py
@@ -1,6 +1,7 @@
 import functools
 from copy import copy
 from enum import Enum
+import re
 
 import numpy as np
 from gymnasium.spaces import Discrete, Dict, MultiDiscrete, MultiBinary, Box, Tuple
@@ -13,12 +14,13 @@ import pygame
 class UAVState(Enum):
     IDLE = 0
     DELIVERING = 1
-    RETURNING = 2
+    LANDING = 2
+    RETURNING = 3
 
 
-class CustomerState(Enum):
-    WAITING = 0
-    DELIVERED = 1
+class TruckState(Enum):
+    MOVING = 0
+    LANDING = 1
 
 
 class DeliveryEnv(ParallelEnv):
@@ -72,6 +74,7 @@ class DeliveryEnv(ParallelEnv):
         # agent definition
         self.possible_agents = ([f"uav_{i}_{j}" for j in range(self.uav_num) for i in range(self.group_num)]
                                 + [f"truck_{i}_{j}" for j in range(self.truck_num) for i in range(self.group_num)])
+        self.agents = copy(self.possible_agents)
         self.agent_name_mapping = dict(
             zip(self.possible_agents, range(len(self.possible_agents)))
         )
@@ -80,44 +83,52 @@ class DeliveryEnv(ParallelEnv):
         self.time_step = 0
         self.nodes_location = None
         self.nodes_weight = None
+        self.node_mask = np.zeros(self.num_customer)
         self.warehouse = (0, 0)
-        self.cur_uav_capacity = {
-            agent: self.uav_capacity for agent in self.possible_agents if agent.startswith("uav")
-        }
-        self.cur_uav_power = {
-            agent: self.uav_power for agent in self.possible_agents if agent.startswith("uav")
-        }
-        self.cur_uav_travel_distance = {
-            agent: 0 for agent in self.possible_agents if agent.startswith("uav")
-        }
-        self.agent_coordinates = {
-            agent: self.warehouse for agent in self.possible_agents
-        }
+        self.cur_uav_capacity = None
+        self.cur_uav_power = None
+        self.cur_uav_travel_distance = None
+        self.agent_coordinates = None
+        self.agent_status = None
+
+        self.agent_target = None
 
         # define the observation space and action space for each agent
         self.observation_spaces = {}
         self.action_spaces = {}
         for agent in self.possible_agents:
             obs = {
+                "action_mask": Discrete(2),
                 "nodes_mask": MultiBinary(self.num_customer),
                 "nodes": Tuple([
                     Box(low=np.array([-self.space_width, -self.space_height]),
                         high=np.array([self.space_width, self.space_height]),
-                        dtype=np.int16)
-                    ] * self.num_customer)
+                        dtype=np.float32)
+                    ] * self.num_customer),
+                "parcel": Tuple([Box(low=0, high=self.uav_capacity, dtype=np.float16)] * self.num_customer),
             }
             if agent.startswith("truck"):
                 obs["uav_status"] = MultiDiscrete(np.array([len(UAVState)] * self.uav_num))
                 self.observation_spaces[agent] = Dict(obs)
                 self.action_spaces[agent] = Discrete(self.num_customer + 1)
             else:
-                obs["truck"] = Box(low=np.array([-self.space_width, -self.space_height]), high=np.array([self.space_width, self.space_height]), dtype=np.int16)
-                obs["coordinate"] = Box(low=np.array([-self.space_width, -self.space_height]), high=np.array([self.space_width, self.space_height]), dtype=np.int16)
+                obs["truck"] = Tuple([
+                    Box(
+                        low=np.array([-self.space_width, -self.space_height]),
+                        high=np.array([self.space_width, self.space_height]),
+                        dtype=np.float32
+                    )
+                ] * self.truck_num)
+                obs["coordinate"] = Box(
+                    low=np.array([-self.space_width, -self.space_height]),
+                    high=np.array([self.space_width, self.space_height]),
+                    dtype=np.float32
+                )
                 obs["power"] = Box(0, high=self.uav_power, dtype=np.float32)
                 obs["capacity"] = Box(0, high=self.uav_capacity, dtype=np.float32)
                 obs["travel_distance"] = Box(0, high=self.space_width * self.space_height, dtype=np.float32),
                 self.observation_spaces[agent] = Dict(obs)
-                self.action_spaces[agent] = Box(low=-1, high=1, shape=(2,), dtype=np.float32)
+                self.action_spaces[agent] = Discrete(self.num_customer + self.truck_num)
 
     @functools.lru_cache(maxsize=None)
     def observation_space(self, agent):
@@ -146,6 +157,7 @@ class DeliveryEnv(ParallelEnv):
         self.time_step = 0
         self.nodes_location = self._generate_nodes()
         self.nodes_weight = self._generate_parcel_weight()
+        self.node_mask = np.zeros(self.num_customer)
         self.warehouse = (0, 0)
         self.cur_uav_capacity = {
             agent: self.uav_capacity for agent in self.possible_agents if agent.startswith("uav")
@@ -159,25 +171,11 @@ class DeliveryEnv(ParallelEnv):
         self.agent_coordinates = {
             agent: self.warehouse for agent in self.possible_agents
         }
+        self.agent_status = {agent: UAVState.IDLE.value if agent.startswith("uav") else TruckState.LANDING.value for agent in self.possible_agents}
 
-        observations = {}
-        for agent in self.possible_agents:
-            obs = {
-                "nodes_mask": np.zeros(self.num_customer),
-                "nodes": self.nodes_location
-            }
-            if agent.startswith("truck"):
-                obs["uav_status"] = np.zeros(self.uav_num)
-                observations[agent] = obs
-            else:
-                obs["truck"] = self.warehouse
-                obs["coordinate"] = self.warehouse
-                obs["power"] = self.uav_power
-                obs["capacity"] = self.uav_capacity
-                obs["travel_distance"] = 0
-                observations[agent] = obs
+        self.agent_target = {agent: None for agent in self.possible_agents}
 
-        # return the initial observations
+        observations = self._get_observations()
         return observations, {}
 
     def step(self, action):
@@ -195,6 +193,55 @@ class DeliveryEnv(ParallelEnv):
         if self.screen is not None:
             pygame.quit()
             self.screen = None
+
+    def _get_observations(self):
+        """get the observations of the agents
+
+        :return: the observations of the agents
+        """
+        observations = {}
+        for agent in self.possible_agents:
+            group_num, agent_num = self._get_agent_group(agent)
+
+            obs = {
+                "action_mask": int(self._get_action_mask(agent)),
+                "nodes_mask": self.node_mask,
+                "nodes": self.nodes_location,
+                "parcel": self.nodes_weight
+            }
+            if agent.startswith("truck"):
+                obs["uav_status"] = np.zeros(self.uav_num)
+                for x in range(self.uav_num):
+                    obs["uav_status"][x] = self.agent_status[f"uav_{group_num}_{x}"]
+                observations[agent] = obs
+            else:
+                obs["truck"] = np.zeros((self.truck_num, 2))
+                for x in range(self.truck_num):
+                    obs["truck"] = np.array(self.agent_coordinates[f"truck_{group_num}_{x}"])
+
+                obs["coordinate"] = self.agent_coordinates[agent]
+                obs["power"] = self.cur_uav_power[agent]
+                obs["capacity"] = self.cur_uav_capacity[agent]
+                obs["travel_distance"] = self.cur_uav_travel_distance[agent]
+                observations[agent] = obs
+        return observations
+
+    def _get_action_mask(self, agent):
+        """get the action mask of the agent.
+
+        - One UAV can move when all the trucks are landing, and it is idle.
+        - One truck can move when all the UAVs are idle, and it is landing.
+
+        :param agent: the name of the agent
+        :return: the action mask of the agent
+        """
+        status = self.agent_status[agent]
+        group_num, agent_num = self._get_agent_group(agent)
+
+        if agent.startswith("truck"):
+            return status == TruckState.LANDING.value and all(self.agent_status[f"uav_{group_num}_{i}"] == UAVState.IDLE.value for i in range(self.uav_num))
+        else:
+            return status == UAVState.IDLE.value and all(self.agent_status[f"truck_{group_num}_{i}"] == TruckState.LANDING.value for i in range(self.truck_num))
 
     def _generate_nodes(self):
         """generate the nodes in the space
@@ -214,3 +261,20 @@ class DeliveryEnv(ParallelEnv):
         :return: the weights of the nodes
         """
         return self.RNG.integers(1, self.uav_capacity // 2, self.num_customer)
+
+    def _move_to_target(self, agent):
+        """move the agent to the target
+
+        :param agent: the name of the agent
+        """
+        pass
+
+    @staticmethod
+    def _get_agent_group(agent):
+        """get the group number of the agent
+
+        :param agent: the name of the agent
+        :return: the group number and agent number of the agent in that group
+        """
+        return map(int, re.findall(r"\d+", agent))
+
